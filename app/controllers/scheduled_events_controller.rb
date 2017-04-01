1ee881c2d8a2a72a46ad1e80749520f578c222c6
aquoss <amberquoss@gmail.com>
2017-04-01 15:49:48 -0700

    adds show method

--- app/controllers/scheduled_events_controller.rb
+++ app/controllers/scheduled_events_controller.rb
@@ -1,9 +1,15 @@
 class ScheduledEventsController < ApplicationController
 
+  def show
+    sorted_schedule = get_scheduled_events
+    render :json sorted_schedule
+  end
+
   def create
     new_event = ScheduledEvent.new(schedule_params)
     if !overlapping?(new_event)
       new_event.save
+      render json: new_event, status: :created
     end
   end
 
@@ -21,5 +27,23 @@ class ScheduledEventsController < ApplicationController
     end
     false
   end
-  
+
+  def get_scheduled_events
+    days_events = ScheduledEvent.find(dog_id: params[:dog_id]).where(date: params[:date])
+    schedule = []
+    days_events.each do |event|
+      event_hash = {
+        type: event.schedulable_type,
+        start_time: event.start_time,
+        end_time: event.end_time,
+        date: event.date,
+      }
+      event_data = event.schedulable
+      event_data_hash = event_data.map(&:attributes)
+      event_hash[:type_description] = event_data_hash
+      schedule << event_hash
+    end
+    schedule.sort_by { |hash| hash[:start_time] }
+  end
+
 end
