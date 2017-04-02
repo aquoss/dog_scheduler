f84e6f721441463c2b9095de92d6dee6d36a3090
aquoss <amberquoss@gmail.com>
2017-04-02 10:44:43 -0700

    debugs delete and update tests

--- spec/controllers/scheduled_events_controller_spec.rb
+++ spec/controllers/scheduled_events_controller_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 RSpec.describe ScheduledEventsController, type: :controller do
   before :each do
-    let(:dog) { Dog.create(name:"Brutus") }
-    let(:meal) { Meal.create(food: "Kibbles n Bits", portion: "2 cups", dog_id: dog.id) }
+    @dog = Dog.create(name:"Brutus")
+    @meal = Meal.create(food: "Kibbles n Bits", portion: "2 cups", dog_id: @dog.id)
   end
 
   describe "POST#create" do
     it "creates a Scheduled Event for a dog" do
       expect do
-        post :create, { start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: meal.id, dog_id: dog.id }
+        post :create, { start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id }
       end.to change { ScheduledEvent.count }.by 1
 
       expect(ScheduledEvent.last.start_time.to_time.to_s).to eq "2017-01-01 08:00:00 +0000"
       expect(ScheduledEvent.last.end_time.to_time.to_s).to eq "2017-01-01 08:15:00 +0000"
       expect(ScheduledEvent.last.date.to_s).to eq "2017-01-01"
       expect(ScheduledEvent.last.schedulable_type).to eq "Meal"
-      expect(ScheduledEvent.last.schedulable_id).to eq meal.id
-      expect(ScheduledEvent.last.dog_id).to eq dog.id
+      expect(ScheduledEvent.last.schedulable_id).to eq @meal.id
+      expect(ScheduledEvent.last.dog_id).to eq @dog.id
     end
 
     it "returns 201 and renders the Scheduled Event attributes" do
-      post :create, { start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: meal.id, dog_id: dog.id }
+      post :create, { start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id }
       expect(response.status).to eq 201
 
       response_json = JSON.parse(response.body)
@@ -30,14 +30,17 @@ RSpec.describe ScheduledEventsController, type: :controller do
       expect(response_json["end_time"].to_time.to_s).to eq "2017-01-01 08:15:00 +0000"
       expect(response_json["date"]).to eq "2017-01-01"
       expect(response_json["schedulable_type"]).to eq "Meal"
-      expect(response_json["schedulable_id"]).to eq meal.id
-      expect(response_json["dog_id"]).to eq dog.id
+      expect(response_json["schedulable_id"]).to eq @meal.id
+      expect(response_json["dog_id"]).to eq @dog.id
     end
   end
 
   describe 'PUT#update' do
-    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: meal.id, dog_id: dog.id)}
-    let(:attributes) {start_time: "2017-01-01 11:00:00 +0000", end_time: "2017-01-01 11:15:00 +0000"}
+    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id)}
+
+    let(:attributes) do
+      { start_time: "2017-01-01 11:00:00 +0000", end_time: "2017-01-01 11:15:00 +0000" }
+    end
 
     it "updates a scheduled event" do
       put :update, id: scheduled_event.id, scheduled_event: attributes
@@ -49,7 +52,7 @@ RSpec.describe ScheduledEventsController, type: :controller do
   end
 
   describe 'DELETE#destroy' do
-    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: meal.id, dog_id: dog.id)}
+    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id)}
 
     it "deletes a scheduled event" do
       expect {
