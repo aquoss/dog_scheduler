9d2f2c658ae2e924eefde98d2409b9bc432484bb
aquoss <amberquoss@gmail.com>
2017-04-02 12:50:20 -0700

    debugs update test

--- spec/controllers/scheduled_events_controller_spec.rb
+++ spec/controllers/scheduled_events_controller_spec.rb
@@ -36,18 +36,29 @@ RSpec.describe ScheduledEventsController, type: :controller do
   end
 
   describe 'PUT#update' do
-    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 08:00:00 +0000", end_time: "2017-01-01 08:15:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id)}
-
-    let(:attributes) do
-      { start_time: "2017-01-01 11:00:00 +0000", end_time: "2017-01-01 11:15:00 +0000" }
-    end
+    let(:scheduled_event) { ScheduledEvent.create(start_time: "2017-01-01 11:35:00 +0000", end_time: "2017-01-01 11:45:00 +0000", date: "2017-01-01", schedulable_type: "Meal", schedulable_id: @meal.id, dog_id: @dog.id)}
 
     it "updates a scheduled event" do
-      put :update, { dog_id: @dog.id, event_id: scheduled_event.id, scheduled_event: attributes }
-      # scheduled_event.reload
+      put :update, { dog_id: @dog.id, event_id: scheduled_event.id, start_time: "2017-01-01 11:40:00 +0000", end_time: "2017-01-01 11:55:00 +0000" }
+      scheduled_event.reload
 
-      expect(ScheduledEvent.start_time.to_time.to_s).to eq "2017-01-01 11:00:00 +0000"
-      expect(ScheduledEvent.end_time.to_time.to_s).to eq "2017-01-01 11:15:00 +0000"
+      expect(scheduled_event.start_time.to_time.to_s).to eq "2017-01-01 11:40:00 +0000"
+      expect(scheduled_event.end_time.to_time.to_s).to eq "2017-01-01 11:55:00 +0000"
+    end
+
+    it "returns 200 and renders the updated event" do
+      put :update, { dog_id: @dog.id, event_id: scheduled_event.id, start_time: "2017-01-01 11:40:00 +0000", end_time: "2017-01-01 11:55:00 +0000" }
+      expect(response.status).to eq 200
+
+      response_json = JSON.parse(response.body)
+      expect(response_json["id"]).to eq scheduled_event.id
+      expect(response_json["start_time"].to_time.to_s).to eq "2017-01-01 11:40:00 +0000"
+      expect(response_json["end_time"].to_time.to_s).to eq "2017-01-01 11:55:00 +0000"
+      expect(response_json["date"]).to eq "2017-01-01"
+      expect(response_json["schedulable_type"]).to eq "Meal"
+      expect(response_json["schedulable_id"]).to eq @meal.id
+      expect(response_json["dog_id"]).to eq @dog.id
+
     end
   end
 
@@ -58,6 +69,21 @@ RSpec.describe ScheduledEventsController, type: :controller do
       expect {
         delete :destroy, { dog_id: @dog.id, event_id: scheduled_event.id }
       }.to change{ ScheduledEvent.count }.by(0)
+    end
+
+    it "returns 200 and renders the deleted event attributes" do
+      delete :destroy, { dog_id: @dog.id, event_id: scheduled_event.id }
+      expect(response.status).to eq 200
+
+      response_json = JSON.parse(response.body)
+      expect(response_json["id"]).to eq scheduled_event.id
+      expect(response_json["start_time"].to_time.to_s).to eq "2017-01-01 08:00:00 +0000"
+      expect(response_json["end_time"].to_time.to_s).to eq "2017-01-01 08:15:00 +0000"
+      expect(response_json["date"]).to eq "2017-01-01"
+      expect(response_json["schedulable_type"]).to eq "Meal"
+      expect(response_json["schedulable_id"]).to eq @meal.id
+      expect(response_json["dog_id"]).to eq @dog.id
+
     end
   end
 
